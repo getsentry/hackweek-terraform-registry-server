2d7dbf31876ce7d0684cdad45f3243a8270d945a
Buck Evan <buck.evan@sentry.io>
2023-08-24 15:28:22 -0500

    fix(mock-modules): now they have a "system" too

--- tests/mock_modules/lib/mk_modules.sh
+++ tests/mock_modules/lib/mk_modules.sh
@@ -4,27 +4,33 @@ set -euxo pipefail
 set -x
 
 : clean slate
-rm -rv ./module-namespace* || true  # no such file
+rm -rv ./namespace-* || true  # no such file
 
 i=0  # a small pseudo-pseudo-random number
 for namespace in a b c; do
-  for name in x y z; do
-    i=$(((i + 23) % 13))
-    for version in \
-      "$(((i % 5) + 1)).0.$((i))" \
-      "$(((i % 5) + 1)).0.$((i + 1))" \
-      "$(((i % 4) + 1)).1.$((i))" \
+  for module in x y z; do
+    for system in \
+        $(((i + 7) % 3 + 1)) \
+        $(((i + 5) % 2 + 1)) \
     ; do
-      module="module-namespace-$namespace/module-name-$name/$version"
-      mkdir -p "$module"
-      cat > "$module/$name.tf" <<EOF
-output "version" { value = "$version" }
+      i=$(((i + 51) % 31 + 1))
+      for version in \
+        "$(((i % 5) + 1)).0.$((i))" \
+        "$(((i % 5) + 1)).0.$((i + 1))" \
+        "$(((i % 4) + 1)).1.$((i))" \
+      ; do
+        moduledir="namespace-$namespace/module-$module/system-$system/$version"
+        mkdir -p "$moduledir"
+        cat > "$moduledir/$module.tf" <<EOF
 output "namespace" { value = "$namespace" }
-output "name" { value = "$name" }
+output "module" { value = "$module" }
+output "system" { value = "$system" }
+output "version" { value = "$version" }
 EOF
-      tar -cvf "$module.tar" "$module"
-      xz "$module.tar"
-      rm -r "$module"
+        tar -cvf "$moduledir.tar" "$moduledir"
+        xz "$moduledir.tar" || true  # file already exists
+        rm -r "$moduledir"
+      done
     done
   done
 done
